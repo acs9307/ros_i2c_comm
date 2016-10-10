a88ceb8e6c9f2b367bcf0f89a03461169b543766
acs9307 <acs9307@hotmail.com>
2016-10-10 16:49:11 +0000

    Modified test code.

--- src/test_i2c_out.cpp
+++ src/test_i2c_out.cpp
@@ -14,27 +14,32 @@ int main(int argc, char** argv)
 	
 	char inBuff[1024] = {0};
 	std::cout<<"Type in the data to send.\n";
-	std::fgets(inBuff, sizeof(inBuff), stdin);
 	
-	/* Remove new line character. */
-	char* it = inBuff + sizeof(inBuff) - 1;
-	for(; it > inBuff; --it)
+	while(1)
 	{
-		if(*it == '\n' || *it == '\r')
+		memset(inBuff, 0, sizeof(inBuff));
+		std::fgets(inBuff, sizeof(inBuff), stdin);
+		
+		/* Remove new line character. */
+		char* it = inBuff + sizeof(inBuff) - 1;
+		for(; it > inBuff; --it)
 		{
-			for(; it > inBuff && (*it == '\n' || *it == '\r'); --it);
-			it[1] = 0;
+			if(*it == '\n' || *it == '\r')
+			{
+				for(; it > inBuff && (*it == '\n' || *it == '\r'); --it);
+				it[1] = 0;
+			}
 		}
-	}
-	args.request.data = inBuff;
-	
-	if(outClient.call(args))
-	{
-		ROS_INFO("Successfully sent \"%s\".", args.request.data.c_str());
-	}
-	else
-	{
-		ROS_INFO("Error occurred sending data.");
+		args.request.data = inBuff;
+		
+		if(outClient.call(args))
+		{
+			ROS_INFO("Successfully sent \"%s\".", args.request.data.c_str());
+		}
+		else
+		{
+			ROS_INFO("Error occurred sending data.");
+		}
 	}
 	
 	return(0);
